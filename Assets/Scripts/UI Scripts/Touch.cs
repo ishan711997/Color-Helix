426eb172742ebb931a27188c7c634c9bd5be14de
ishan711997 <ishan711997@gmail.com>
2021-05-14 18:01:08 +0530

    first commit

---
+++ Assets/Scripts/UI Scripts/Touch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Touch : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+    private static bool pressing;
+
+    public static bool IsPressing()
+    {
+        return pressing;
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        pressing = true;
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        pressing = false;
+    }
+
+}
